defd5dad3ecfd647588c3c35768ed4c3fa4fe1d1
Maxim Zabolotny <maxim.zabolotny20121776@gmail.com>
2020-12-16 13:59:01 +0200

    completed tiny-js-world task

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
+  const phrases = {
+   cat: 'Meow!',
+   dog: 'bark-bark!',
+   man: 'Olla ',
+   woman: 'Hi ',
+ };
+ 
+ const cat = {
+   species: 'cat',
+   name: 'Alex',
+   legs: 4,
+   saying: phrases.cat,
+   gender: 'male',
+   friends: 'Kevin, Anna',
+ };
+ const catWoman = {
+   species: 'cat',
+   name: 'Mike',
+   legs: 4,
+   saying: phrases.cat,
+   gender: 'female',
+   friends: 'Alex',
+ };
+ const dog = {
+   species: 'dog',
+   name: 'Max',
+   legs: 4,
+   saying: phrases.dog,
+   gender: 'male',
+   friends: 'Anna, Alex',
+ };
+ const man = {
+   species: 'human',
+   name: 'Elon',
+   hands: 2,
+   legs: 2,
+   saying: phrases.man,
+   gender: 'male',
+   friends: 'Alex, Anna',
+ };
+ const woman = {
+   species: 'human',
+   name: 'Anna',
+   hands: 2,
+   legs: 2,
+   saying: phrases.woman,
+   gender: 'female',
+   friends: 'Alex, Elon',
+ };
+ const inhabitants = [cat, catWoman, man, woman, dog];
+ 
+ const makeFormatOutput = ({
+   name,
+   species,
+   hands,
+   legs,
+   saying,
+   gender,
+   friends,
+ }) => {
+   return `Hello! I am ${species}, my name is ${name}, my gender is ${gender}, I have ${hands || '0'} arms and ${legs} legs and you know what: ${saying}. My friends: ${friends}`;
+ };
+ 
+ inhabitants.forEach((habitant) => print(makeFormatOutput(habitant), 'h5'));
+ 
