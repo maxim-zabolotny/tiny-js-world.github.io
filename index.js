fe6220397638116c915b82d11a5c0e3883b3d1d7
Maxim Zabolotny <maxim.zabolotny20121776@gmail.com>
2020-12-20 20:53:48 +0200

    completed task OOP exercise

--- index.js
+++ index.js
@@ -1,73 +1,79 @@
-  const phrases = {
-   cat: 'Meow!',
-   dog: 'bark-bark!',
-   man: 'Hello',
-   woman: 'Hi ',
- };
- 
- const cat = {
-   species: 'cat',
-   name: 'Alex',
-   legs: 4,
-   saying: phrases.cat,
-   gender: 'male',
-   friends: 'Kevin, Anna',
- };
- const catWoman = {
-   species: 'cat',
-   name: 'Mike',
-   legs: 4,
-   saying: phrases.cat,
-   gender: 'female',
-   friends: 'Alex',
- };
- const dog = {
-   species: 'dog',
-   name: 'Max',
-   legs: 4,
-   saying: phrases.dog,
-   gender: 'male',
-   friends: 'Anna, Alex',
- };
- const man = {
-   species: 'human',
-   name: 'Elon',
-   hands: 2,
-   legs: 2,
-   saying: phrases.man,
-   gender: 'male',
-   friends: 'Alex, Anna',
- };
- const woman = {
-   species: 'human',
-   name: 'Anna',
-   hands: 2,
-   legs: 2,
-   saying: phrases.woman,
-   gender: 'female',
-   friends: 'Alex, Elon',
- };
- const inhabitants = [cat, catWoman, man, woman, dog];
- 
- const makeFormatOutput = ({
-   name,
-   species,
-   hands,
-   legs,
-   saying,
-   gender,
-   friends,
- }) => {
-   return [
-     `${saying}, `,
-     `I am ${species}, `,
-     `my name is ${name}, `,
-     `my gender is ${gender}, `,
-     `I have ${hands || '0'} arms `,
-     `and ${legs} legs. `,
-     `My friends: ${friends}.`,
-   ].join(' ')
- };
- 
- inhabitants.forEach((habitant) => print(makeFormatOutput(habitant), 'h5'));
- 
+class Creature {
+  constructor(name, gender, species, legs, saying) {
+    this.name = name;
+    this.gender = gender;
+    this.species = species;
+    this.legs = legs;
+    this.saying = saying;
+    this.friends = [];
+  }
+
+  addFriends(...newFriends) {
+    newFriends.forEach(({ name }) => this.friends.push(name));
+    return this.friends;
+  }
+
+  toString() {
+    let template = [
+      `name: ${this.name}; `,
+      `gender: ${this.gender}; `,
+      `species: ${this.species}; `,
+      `saying: ${this.saying}; `,
+      `legs: ${this.legs}; `,
+    ];
+
+    if (this.friends.length > 0) {
+      template.push(`friends: ${this.friends}; `);
+    }
+    return template.join('');
+  }
+}
+
+class Dog extends Creature {
+  constructor(name, gender) {
+    super(name, gender, 'dog', 4, 'woof!');
+  }
+}
+
+class Cat extends Creature {
+  constructor(name, gender) {
+    super(name, gender, 'cat', 4, 'mew-mew-mew-mew');
+  }
+}
+
+class Human extends Creature {
+  constructor(name, gender, saying) {
+    super(name, gender, 'homo sapiens', 2, saying);
+    this.hands = 2;
+  }
+
+  toString() {
+    return [super.toString(), `hands: ${this.hands}`].join('');
+  }
+}
+
+class Woman extends Human {
+  constructor(name) {
+    super(name, 'female', 'I am a woman.');
+  }
+}
+
+class Man extends Human {
+  constructor(name) {
+    super(name, 'male', 'I am a man.');
+  }
+}
+
+const dog = new Dog('Alex', 'male');
+const cat = new Cat('Dora', 'female');
+const woman = new Woman('Anna');
+const man = new Man('Elon');
+man.addFriends(cat, woman);
+woman.addFriends(man, dog);
+dog.addFriends(cat, woman, man);
+
+const creatures = [dog, cat, woman, man];
+
+creatures.forEach((creature) => {
+  print(creature);
+});
